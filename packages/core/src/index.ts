1d5a0bb1d8d97f1e25d1e22ff67711925da78be7
Leo <leoli_082000@hotmail.com>
2026-07-31 06:57:27 +0800

    Export completion hint protocol

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MPL-2.0
 export * from "./legacy-index.js";
+export * from "./completion-hints.js";
 export { fingerprintShape } from "./fingerprint.js";
 export type { FingerprintShapeOptions } from "./fingerprint.js";
