45104230aa514c0e49ed66d01fc3ed2843ecfb62
Leo <cheerleaderleo@outlook.com>
2026-08-27 01:12:09 +0800

    Expose lane governor from core index

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MPL-2.0
 export * from "./legacy-index.js";
 export * from "./completion-hints.js";
+export * from "./lane-governor.js";
 export { fingerprintShape } from "./fingerprint.js";
 export type { FingerprintShapeOptions } from "./fingerprint.js";
