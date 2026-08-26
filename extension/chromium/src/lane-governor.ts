2ff6fa1a2caa2b62b956e150d62dcd572be94606
Leo <cheerleaderleo@outlook.com>
2026-08-27 01:28:28 +0800

    Bridge Chromium host to lane governor

---
+++ extension/chromium/src/lane-governor.ts
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MPL-2.0
+export {
+  evaluateLane,
+  parseLaneSignals,
+  type LaneDecision,
+  type LaneLifecycle,
+  type LaneSignals,
+} from "../../../packages/core/src/lane-governor.js";
